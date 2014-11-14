bf72d78ee6e9e612276ee4177fb406a189d84814
waivek <waivek@gmail.com>
2014-11-14 14:56:23 +0300

    Added First Functioning of Youtube-DL

--- youtube-playlist-dl/youtube-dl.pl
+++ youtube-playlist-dl/youtube-dl.pl
@@ -1,9 +1,28 @@
-use List::MoreUtils natatime;
-print "<html>\n";
-my $it = natatime 2, <STDIN>;
-while(my ($x, $y) = $it->()) {
-    chomp $x;
-    chomp $y;
-    print "\t<a href=$x>$y</a></br>\n";
+print @ARGV;
+my $command = q[youtube-dl -f 135 --get-filename -g --ignore-config -o "%(playlist_index)s-%(title)s" https://www.youtube.com/playlist?list=PLckFgM6dUP2hc4iy-IdKFtqR9TeZWMPjm|];
+open(PIPE, $command);
+# my $it = natatime 2, <PIPE>;
+# print "vivek\n";
+# while(my ($x, $y) = $it->()) {
+#     chomp $x;
+#     chomp $y;
+#     print "\t<a href=$x>$y</a></br>\n";
+# }
+my $i = 0;
+print "Started!\n";
+open my $fh, '>', "links.html";
+print {$fh} "<html>\n";
+while(<PIPE>) {
+    chomp;
+    if($i eq 0) {
+        print {$fh} "\t<a href=$_>";
+    }
+    else {
+        print {$fh} "$_</a></br>\n";
+        print "Completed: $_\n";
+    }
+    $i = ($i + 1) % 2;
 }
-print "</html>";
+print {$fh} "</html>";
+close $fh;
+close(PIPE);
